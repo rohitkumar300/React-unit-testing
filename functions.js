0cefd4950521d24cfd0618716ed8a7e47b7ce0f9
rohitkumar300 <rohit@ikshalabs.com>
2022-10-31 14:11:47 +0530

    Added 7 tests

--- functions.js
+++ functions.js
@@ -1,6 +1,24 @@
+const { axios } = require("axios");
+
 const functions = {
     add: function(num1,num2){
         return num1+num2;
+    },
+    isNull: function(){
+        return null
+    },
+    createUser: function(){
+        const user = {'FirstName': "Rohit"};
+        user['LastName'] = "Kumar";
+        return user;
+    },
+    marksCheck: function(marks1,marks2){
+        return marks1+marks2;
+    },
+    fetchUser: function(){
+        axios.get("https://jsonplaceholder.typicode.com/users/1")
+        .then((res)=>{res.data})
+        .catch((err)=>{'Error'})
     }
 }
 
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,44 @@
 const functions = require('./functions')
-
+//toBe
 test("Adds 2 + 2 to equal 4",()=>{
-    expect(functions.add(2,2)).toBe(5)
+    expect(functions.add(2,2)).toBe(4)
 })
+
+test("Adds 2 + 2 to not equal 5",()=>{
+    expect(functions.add(2,2)).not.toBe(5)
+})
+//Falsy values
+test("Should be null",()=>{
+    expect(functions.isNull()).toBeNull()
+})
+//toEqual
+test("Should return Rohit Kumar",()=>{
+    expect(functions.createUser()).toEqual({FirstName: "Rohit",LastName: "Kumar"})
+})
+
+//toBeLessThan toBeLessThanOrEqual
+test("Should have less than 200",()=>{
+    expect(functions.marksCheck(100,90)).toBeLessThan(200);
+})
+
+//regex (toMatch)
+test("There is no I in team",()=>{
+    expect('team').not.toMatch(/I/)
+})
+
+//Arrays (toContain)
+test('Human should be in species',()=>{
+    species = ['Cat','Dog','Human'];
+    expect(species).toContain('Human')
+})
+
+//Working with async data
+test("User name should be leanne Graham",()=>{
+    // expect.assertions(1)
+    return functions.fetchUser()
+    .then(data=>{
+        expect(data.name).toEqual('Leanne Graham')
+    })
+})
+
+//async await
